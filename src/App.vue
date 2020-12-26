57ec7b706cd6ed0a9a9bf9126fb7036ed8bdc829
vin-vlad <palamarchuk.gg@gmail.com>
2020-12-26 21:03:55 +0200

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+       <span>Spectroscope</span>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,190 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="800"
+    max-height="800"
+    elevation="10"
+  >
+    <v-img
+      src="../assets/Spectrograph.png"
+    ></v-img>
+    <v-img
+      src="../assets/graduation.png"
+    ></v-img>
+
+    <v-toolbar
+      flat
+    >
+      <v-toolbar-title>
+        <span class="subheading">Spectroscope value: </span> <span  class="display-1 font-weight-light"
+            v-text="calculateScale"
+          ></span>
+        
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      
+    </v-toolbar>
+    
+         
+    <v-card-text>
+       <v-slider
+        v-model="linearScale"
+        track-color="grey"
+        always-dirty
+        step="1"
+        :min="minLinearScale"
+        :max="maxLinearScale"
+        :thumb-size="24"
+        thumb-label="always"
+      >
+      </v-slider>
+       </v-card-text>
+
+    <v-card-text>
+       <v-slider
+        v-model="circularScale"
+        track-color="grey"
+        always-dirty
+        step="1"
+        :min="minCircularScale"
+        :max="maxCircularScale"
+        :thumb-size="24"
+        thumb-label="always"
+      >
+       
+      </v-slider>
+    </v-card-text>
+
+    <v-card-text>
+      <v-row align="center">
+
+      <v-col
+        class="d-flex"
+        cols="12"
+        sm="6"
+      >
+        <v-select
+          v-model="gas"
+          :items="specters"
+          item-text="name"
+          item-value="icon"
+          label="Select gas..."
+          outlined
+        ></v-select>
+      </v-col>
+
+      
+    </v-row>
+    </v-card-text>
+
+    <v-card-text>
+      <v-row
+        class="mb-4"
+        justify="space-between"
+      >
+        <v-col class="text-left">
+          <span
+            class="display-3 font-weight-light"
+            v-text="calculateWl"
+          ></span>
+          <span class="subheading font-weight-light mr-1">nm</span>
+          <v-fade-transition>
+            <v-avatar
+              v-if="isPlaying"
+              :color="color"
+              :style="{
+                animationDuration: animationDuration
+              }"
+              class="mb-1 v-avatar--metronome"
+              size="12"
+            ></v-avatar>
+          </v-fade-transition>
+        </v-col>
+        <v-col class="text-right">
+        </v-col>
+      </v-row>
+
+      <v-slider
+        v-model="calculateWl"
+        :color="color"
+        track-color="grey"
+        readonly
+        always-dirty
+        :min="minWl"
+        :max="maxWl"
+        :thumb-size="24"
+        thumb-label="always"
+      >
+       
+      </v-slider>
+
+      <v-img
+      :width="770"
+      :height="170"
+      :src="gas"
+      ></v-img>
+    </v-card-text>
+
+
+  </v-card>
+</template>
+<script>
+  export default {
+    name: "HelloWorld",
+    data: () => ({
+      gas: null,
+      bpm: 0,
+      linearScale: 6,
+      maxLinearScale: 6,
+      minLinearScale: 1,
+      circularScale: 20,
+      minCircularScale: 0,
+      maxCircularScale: 50,
+      minWl: 380,
+      maxWl: 750,
+      interval: null,
+      isPlaying: false,
+      spectroscopeScale: 3,
+      imgSrc:"https://i.imgur.com/b0dnEv9.png",
+      specters: [
+        {name:"Hydrogen",icon:"https://i.imgur.com/WuqRrD7.png"},
+        {name:"Neon",icon:"https://i.imgur.com/UMplp82.png"},
+        {name:"Helium",icon:"https://i.imgur.com/b0dnEv9.png"}
+      ]
+    }),
+
+    computed: {
+      color () {
+        if (this.bpm >= 380 && this.bpm <= 450) return 'purple'
+        if (this.bpm > 450 && this.bpm <= 485) return 'blue'
+        if (this.bpm > 485 && this.bpm <= 500) return 'cyan'
+        if (this.bpm > 500 && this.bpm <= 565) return 'green'
+        if (this.bpm > 565 && this.bpm <= 590) return 'yellow'
+        if (this.bpm > 590 && this.bpm <= 625) return 'orange'
+        if (this.bpm > 625 && this.bpm <= 750) return 'red'
+        return 'red'
+      },
+      calculateWl (){
+        return Math.round(((this.circularScale*0.02+this.linearScale)/(this.maxLinearScale+this.maxCircularScale*0.02)) * this.maxWl)
+      },
+      calculateScale (){
+        return this.circularScale*0.02+this.linearScale
+      },
+      animationDuration () {
+        return `${60 / this.bpm}s`
+      },
+    },
+
+    methods: {
+      decrement () {
+        this.bpm--
+      },
+      increment () {
+        this.bpm++
+      },
+      toggle () {
+        this.isPlaying = !this.isPlaying
+      },
+    },
+  }
+</script>
